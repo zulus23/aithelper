dce3b1d676a2531050e3cdf68bacd843325983ee
zulus23 <zulus23@gmail.com>
2018-02-02 16:47:30 +0300

    add template code create data service

--- src/main/java/ru/zhukov/ait/dao/ApplicationServiceImpl.java
+++ src/main/java/ru/zhukov/ait/dao/ApplicationServiceImpl.java
@@ -8,9 +8,14 @@ import ru.zhukov.ait.domain.TypeOrder;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.WeakHashMap;
 
 @Service
 public class ApplicationServiceImpl implements ApplicationService {
+
+    private final  Map<Enterprise,ApplicationDataService> dataServiceMap = new WeakHashMap<>();
+
     @Override
     public List<Enterprise> listEnterprise() {
         return Arrays.asList(Enterprise.values());
@@ -21,7 +26,7 @@ public class ApplicationServiceImpl implements ApplicationService {
 
         switch (enterprise){
             case GOTEK:{
-
+                dataServiceMap.computeIfAbsent(enterprise,(e)-> null);
             }
             case POLYPACK:{
 
