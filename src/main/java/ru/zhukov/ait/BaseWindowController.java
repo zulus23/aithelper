325cf8c6ab6d8ff3193f125db3a2e7f076679756
zulus23 <zulus23@gmail.com>
2018-02-02 12:28:16 +0300

    add fill list in combobox

--- src/main/java/ru/zhukov/ait/BaseWindowController.java
+++ src/main/java/ru/zhukov/ait/BaseWindowController.java
@@ -1,13 +1,51 @@
 package ru.zhukov.ait;
 
+import javafx.beans.Observable;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.util.StringConverter;
+import ru.zhukov.ait.dao.ApplicationService;
+import ru.zhukov.ait.domain.Enterprise;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class BaseWindowController implements Initializable {
+
+    private ApplicationService  applicationService;
+
+    @FXML
+    private ComboBox<Enterprise> aitEnterprise;
+
+    public BaseWindowController(ApplicationService applicationService) {
+        this.applicationService = applicationService;
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        System.out.println(applicationService.listEnterprise());
+        aitEnterprise.getItems().addAll(applicationService.listEnterprise());
+        aitEnterprise.setConverter(new StringConverter<Enterprise>() {
+            @Override
+            public String toString(Enterprise enterprise) {
+                return enterprise.getNameEnterprise();
+            }
+
+            @Override
+            public Enterprise fromString(String enterpriseName) {
+                return aitEnterprise.getItems().filtered(e-> e.getNameEnterprise().equals(enterpriseName)).get(0);
+            }
+        });
+        aitEnterprise.getSelectionModel().selectedItemProperty().addListener(this::enterpriseChanged);
+
+    }
+
+
+
+    private void enterpriseChanged(ObservableValue<? extends  Enterprise> observable,Enterprise oldEnterprise,Enterprise newEnterprise) {
+
 
     }
 }
--- src/main/java/ru/zhukov/ait/Bootstrap.java
+++ src/main/java/ru/zhukov/ait/Bootstrap.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.zhukov.ait.config.ApplicationConfig;
+import ru.zhukov.ait.dao.ApplicationService;
 
 import java.io.IOException;
 import java.net.URL;
@@ -29,13 +30,14 @@ public class Bootstrap extends Application {
 
 
         private void showWindow(){
-            FXMLLoader fxmlLoader = new FXMLLoader();
+            FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("BaseWindow.fxml"));
             ResourceBundle resourceBundle = ResourceBundle.getBundle("aithelper");
             Locale.setDefault(new Locale("ru","RU"));
-            BaseWindowController controller = new BaseWindowController();
+            BaseWindowController controller = new BaseWindowController(context.getBean(ApplicationService.class));
             fxmlLoader.setController(controller);
+            fxmlLoader.setResources(resourceBundle);
             try {
-                AnchorPane anchorPane = fxmlLoader.load(this.getClass().getResource("BaseWindow.fxml"),resourceBundle);
+                AnchorPane anchorPane = fxmlLoader.load();
                 Scene scene = new Scene(anchorPane);
                 stage.setScene(scene);
                 stage.setTitle(resourceBundle.getString("main.window.title"));
